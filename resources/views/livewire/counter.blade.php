fc1da0c0a54d3c02436ed152d8b70ef6288ae822
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-06-29 18:40:02 +0400

    Add styles to counter component

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,5 @@
-<div>
-    <button wire:click="increment">+</button>
+<div class="p-16 flex justify-center gap-6 items-center">
+    <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">+</button>
     {{ $count }}
-    <button wire:click="decrement">-</button>
+    <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">-</button>
 </div>
