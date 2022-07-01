c3ff87d08fd5a4131e3bae823bfd47f1b1b10d2d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-01 09:51:23 +0400

    Remove unnecessary classes from todo list button

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -2,7 +2,7 @@
     <div class="flex gap-4 justify-between">
         <input type="text" wire:model="todoText" wire:keydown.enter="addTodo" placeholder="Type and hit enter" class="flex-1">
         <button
-            class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 disabled:cursor-not-allowed disabled:bg-opacity-90 rounded text-white"
+            class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white"
             wire:click="addTodo"
         >Add
         </button>
